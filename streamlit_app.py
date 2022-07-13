f007d8134508fc1f94076edddcd87b84988d4a92
Mateusz Buziak <mateusz.buziak@pfizer.com>
2022-07-13 11:42:48 +0200

    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text("test test test")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
-streamlit.dataframe(fruit_list) 
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list) 
 
