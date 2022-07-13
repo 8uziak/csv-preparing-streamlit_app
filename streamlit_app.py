0f6f74b02c459d861c71f6d545a3d0a20f79acdd
Mateusz Buziak <mateusz.buziak@pfizer.com>
2022-07-13 11:29:14 +0200

    second header added with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header("Pancakes recipe")
 streamlit.text("1 cup of milk")
 streamlit.text("3 table spoons of ...")
 streamlit.text("test test test")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
