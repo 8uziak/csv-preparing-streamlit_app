e9d0edcc3ef97402c3c09a0c9d4269e1dfdebbc0
Mateusz Buziak <mateusz.buziak@pfizer.com>
2022-07-13 11:32:55 +0200

    adding table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit 
+import pandas as pd
 
 streamlit.title("Hello World")
 streamlit.header("Pancakes recipe")
@@ -7,3 +8,7 @@ streamlit.text("3 table spoons of ...")
 streamlit.text("test test test")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+streamlit.dataframe(fruit_list) 
+
