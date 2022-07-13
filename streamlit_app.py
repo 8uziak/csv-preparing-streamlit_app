aac6eb67a3b91cffd4b4f3c09cc0d7ef0877bd46
Mateusz Buziak <mateusz.buziak@pfizer.com>
2022-07-13 10:26:32 +0200

    added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit 
 
 streamlit.title("Hello World")
+streamlit.header("Pancakes recipe")
+streamlit.text("1 cup of milk")
+streamlit.text("3 table spoons of ...")
+streamlit.text("test test test")
