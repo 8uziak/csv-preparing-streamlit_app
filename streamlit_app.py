e8a857b1faa057b0c25090313381336c55b64312
Mateusz Buziak <mateusz.buziak@pfizer.com>
2022-07-18 20:56:12 +0200

    preparing table

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,11 @@ streamlit.header("(cleaning CSV table)")
 streamlit.text("columns are split incorrectly, commas should be used, not semicolons")
 streamlit.text("there are 1 column and 160735 rows in the table now, there should be 3 rows ")
 streamlit.text("after the columns are split")
-my_fruit_list = pd.read_csv("https://raw.githubusercontent.com/8uziak/Python-Panda-Library/main/cleaning%20CSV%20table/BCL_TaskCreation.csv") 
-streamlit.dataframe(my_fruit_list) 
+df = pd.read_csv("https://raw.githubusercontent.com/8uziak/Python-Panda-Library/main/cleaning%20CSV%20table/BCL_TaskCreation.csv") 
+streamlit.dataframe(df) 
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/8uziak/Python-Panda-Library/main/cleaning%20CSV%20table/BCL_TaskCreation.csv',delimiter=';')
+streamlit.dataframe(df) 
 
 streamlit.text("!")
